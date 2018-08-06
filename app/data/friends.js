2beeb18cb8cc6e349304c3e3ec1e801899330b6d
Prestonmw <prestonwestmoreland@yahoo.com>
2018-08-06 00:54:04 -0400

    edits-buttons not directing to proper stuff

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,7 @@
 // Data structure that will store the friends data
 var friendsArray = [
 	{
-		name: 'Happy',
-		photo: 'https://cdn.britannica.com/700x450/53/194353-004-DAD1EA1B.jpg',
+		name: 'Mario',
 		scores: [
 			'1',
 			'5',
@@ -17,8 +16,7 @@ var friendsArray = [
 		]
 	},
 	{
-		name: 'Fluffy',
-		photo: 'https://i.ytimg.com/vi/mRf3-JkwqfU/hqdefault.jpg',
+		name: 'Luigi',
 		scores: [
 			'2',
 			'4',
@@ -33,8 +31,7 @@ var friendsArray = [
 		]
 	},
 	{
-		name: 'Spencer',
-		photo: 'http://cdn3-www.cattime.com/assets/uploads/2011/08/best-kitten-names-1.jpg',
+		name: 'Peach',
 		scores: [
 			'4',
 			'3',
@@ -49,8 +46,7 @@ var friendsArray = [
 		]
 	},
 	{
-		name: 'Zackariah',
-		photo: 'https://pbs.twimg.com/profile_images/562466745340817408/_nIu8KHX.jpeg',
+		name: 'Daisy',
 		scores: [
 			'5',
 			'1',
@@ -65,8 +61,7 @@ var friendsArray = [
 		]
 	},
 	{
-		name: 'Cloe',
-		photo: 'http://4.bp.blogspot.com/-HTvSYzA-pO4/UgQb4Zh_u0I/AAAAAAAAEuI/XwhtogT_1tA/s1600/3+cute2.jpg',
+		name: 'Rosalina',
 		scores: [
 			'1',
 			'1',
@@ -81,8 +76,7 @@ var friendsArray = [
 		]
 	},
 	{
-		name: 'Mrs. Piggy',
-		photo: 'http://img.mindbodygreen.com/image/upload/c_limit,w_738,f_auto/ftr/Miss-Piggy-large-tonya-312.jpg',
+		name: 'Wario',
 		scores: [
 			'1',
 			'1',
@@ -97,8 +91,7 @@ var friendsArray = [
 		]
 	},
 	{
-		name: 'Beyonce Knowles',
-		photo: 'http://www.billboard.com/files/styles/article_main_image/public/media/beyonce-feb-2014-billboard-650.jpg',
+		name: 'Waluigi',
 		scores: [
 			'5',
 			'4',
@@ -113,8 +106,7 @@ var friendsArray = [
 		]
 	},
 	{
-		name: 'Adele Adkins',
-		photo: 'https://i1.imgiz.com/data/videoshots/6829/6829475-6461.jpg',
+		name: 'Bowser',
 		scores: [
 			'5',
 			'2',
@@ -129,8 +121,7 @@ var friendsArray = [
 		]
 	},
 	{
-		name: 'Kermit the Frog',
-		photo: 'http://i.dailymail.co.uk/i/pix/2015/09/02/08/2BE5406100000578-3218820-image-m-25_1441179775589.jpg',
+		name: 'Bayonetta
 		scores: [
 			'5',
 			'5',
@@ -145,8 +136,7 @@ var friendsArray = [
 		]
 	},
 	{
-		name: 'Elmo',
-		photo: 'http://1rjm5qg4v8732kpmn44ya6bj-wpengine.netdna-ssl.com/wp-content/uploads/2012/11/elmo-6601.png',
+		name: 'Samus Aran',
 		scores: [
 			'1',
 			'1',
