8e1cdd1022acda2d0365c05f5dc93606e4cda35c
Prestonmw <prestonwestmoreland@yahoo.com>
2018-08-05 20:09:51 -0400

    intial build

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,32 @@
+const friends = require("../data/friends");
+const newUser = require("../public/survey.html");
+
+module.exports = function(app) {
+
+    app.get('/api/friends',function(req,res){
+        res.json(friends)
+    });
+  
+
+    app.post('/api/new',function(req,res){
+
+        var compatibilityScore = 0;
+        var differences = [];
+        //var newUser = req.body;
+
+        for(var i = 0; i < friends.length; i++) {
+            
+            
+            for (var j = 0; j < newUser.userScores.length; j++) {
+                var friendScore = friends.scores[i];
+                var userScore = newUser.scores[j];
+                var difference = friendScore - userScore;
+                var scoreDifference = Math.abs(difference);
+            };
+
+            differences.push(scoreDifference);
+        };
+        
+ });
+ 
+};
