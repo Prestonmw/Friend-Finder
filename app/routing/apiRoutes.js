27707256f6d7a7528a093c45642e83456fc80621
Prestonmw <prestonwestmoreland@yahoo.com>
2018-08-11 12:31:18 -0400

    more edits

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,3 @@
-// A GET route with the url /api/friends. 
-// 	This will be used to display a JSON of all possible friends.
-// A POST routes /api/friends. 
-// 	This will be used to handle incoming survey results. 
-// 	This route will also be used to handle the compatibility logic.
-
-//These routes for our data. Help determine data user sees and data can post
-
 var friendsData = require ('../data/friends.js');
 
 module.exports = function (app) {
@@ -15,10 +7,8 @@ module.exports = function (app) {
 
 
 	app.post('/api/friends', function(req, res){
-		// if(friendsData.length<5) {
-			friendsData.push(req.body);
+				friendsData.push(req.body);
 			res.json(true);
-		// }
 	});
 
 }
