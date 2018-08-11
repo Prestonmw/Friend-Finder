93b6a3dc6bac8a0bbfa44927947bb35828ddec53
Prestonmw <prestonwestmoreland@yahoo.com>
2018-08-11 13:27:00 -0400

    npm packages finally!

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,12 @@
 var friendsData = require ('../data/friends.js');
 
 module.exports = function (app) {
-	app.get('/api/friends', function(req, res) {
+	app.get('/data/friends', function(req, res) {
 		res.json(friendsData);
 	});
 
 
-	app.post('/api/friends', function(req, res){
+	app.post('/data/friends', function(req, res){
 				friendsData.push(req.body);
 			res.json(true);
 	});
